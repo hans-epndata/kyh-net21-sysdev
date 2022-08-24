31714a98674eff5704f11e4f9bc79b0784e3a5da
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2022-08-24 14:05:13 +0200

    Lektion 1

---
+++ Lektion-1/WebApi/Models/Entities/UserEntity.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace WebApi.Models.Entities
+{
+
+    [Index(nameof(Email), IsUnique=true)]
+    public class UserEntity
+    {
+        public UserEntity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required, Column(TypeName = "nvarchar(50)")]
+        public string FirstName { get; set; } = null!;
+
+        [Required, Column(TypeName = "nvarchar(50)")]
+        public string LastName { get; set; } = null!;
+
+        [Required]
+        public string Email { get; set; } = null!;
+
+        [Required, Column(TypeName = "varbinary(max)")]
+        public byte[] Hash { get; private set; } = null!;
+
+        [Required, Column(TypeName = "varbinary(max)")]
+        public byte[] Salt { get; private set; } = null!;
+
+        public void CreatePassword(string password)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                Salt = hmac.Key;
+                Hash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        public bool ValidatePassword(string password)
+        {
+            using (var hmac = new HMACSHA512(Salt))
+            {
+                var _hash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+                for (int i = 0; i < Hash.Length; i++)
+                    if (_hash[i] != Hash[i])
+                        return false;
+
+                return true;
+            }
+        }
+    }
+}
---
+++ Lektion-1/WebApi/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using WebApi.Helpers;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("Sql")));
+
+
+
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();

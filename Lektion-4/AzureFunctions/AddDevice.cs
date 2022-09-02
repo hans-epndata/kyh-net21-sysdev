1ff132a2ee21ac9b49bf8a1e259842a73e81d3ad
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2022-09-02 09:44:32 +0200

    Lektion 4 (Azure function, IotHub)

---
+++ Lektion-4/AzureFunctions/AddDevice.cs
@@ -0,0 +1,37 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Microsoft.Azure.Devices;
+using AzureFunctions.Models;
+
+namespace AzureFunctions
+{
+    public static class AddDevice
+    {
+        private static readonly RegistryManager registryManager =
+            RegistryManager.CreateFromConnectionString(Environment.GetEnvironmentVariable("IotHub"));
+
+
+        [FunctionName("AddDevice")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "devices")] HttpRequest req,
+            ILogger log)
+        {
+            var data = JsonConvert.DeserializeObject<DeviceRequest>(await new StreamReader(req.Body).ReadToEndAsync());
+
+            if (!string.IsNullOrEmpty(data.DeviceId))
+            {
+                var device = await registryManager.AddDeviceAsync(new Device(data.DeviceId));
+                return new OkObjectResult(device);
+            }
+
+            return new BadRequestResult();
+        }
+    }
+}

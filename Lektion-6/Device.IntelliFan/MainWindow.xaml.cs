9d1da8ba91219a3323b8f4ad42651f9785220ca1
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2022-09-09 16:12:51 +0200

    Lektion 6 (AzureFunctions WPF Device)

--- Lektion-6/Device.IntelliFan/MainWindow.xaml.cs
+++ Lektion-6/Device.IntelliFan/MainWindow.xaml.cs
@@ -20,17 +20,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfShared.Helpers;
 
 namespace Device.IntelliFan
 {
-    enum ConnectionState
-    {
-        NotConnected,
-        Connecting,
-        StillConnecting,
-        Initializing,
-        Connected
-    }
 
 
     public partial class MainWindow : Window
@@ -38,114 +31,39 @@ namespace Device.IntelliFan
         public MainWindow()
         {
             InitializeComponent();
-            ConnectAsync().ConfigureAwait(false);
+            UpdateConnectionState().ConfigureAwait(false);
+
+            DeviceManager.Initialize("intellifan-e001", "FAN", "Hans");
+            DeviceManager.ConnectAsync().ConfigureAwait(false);
         }
 
-        private int interval = 10000;
-        private bool isConnected;
-        private string deviceId = "intellifan-e001";
-        private DeviceClient deviceClient;
 
-        private void SetConnectionState(ConnectionState state)
+        private async Task UpdateConnectionState()
         {
-            switch(state)
+            while(true)
             {
-                case ConnectionState.NotConnected:
-                    tblockConnectionState.Text = "Not connected";
-                    btnToggle.Visibility = Visibility.Visible;
-                    btnToggle.Content = "Connect";
-                    break;
-
-                case ConnectionState.Connecting:
-                    tblockConnectionState.Text = "Connecting. Please wait...";
-                    btnToggle.Visibility = Visibility.Hidden;
-                    break;
-
-                case ConnectionState.StillConnecting:
-                    tblockConnectionState.Text = "Still connecting. Please wait...";
-                    btnToggle.Visibility = Visibility.Hidden;
-                    break;
-
-                case ConnectionState.Initializing:
-                    tblockConnectionState.Text = "Initializing. Please wait...";
-                    btnToggle.Visibility = Visibility.Hidden;
-                    break;
-
-                case ConnectionState.Connected:
-                    isConnected = true;
-                    tblockConnectionState.Text = "Connected";
+                tblockConnectionState.Text = DeviceManager.ConnectionStateMessage;
+                if (DeviceManager.ConnectionStateMessage == "Connected")
+                {
                     btnToggle.Visibility = Visibility.Visible;
                     btnToggle.Content = "Start Fan";
-
-                    break;
-
-            }
-        }
-
-        private async Task ConnectAsync()
-        {
-            for (int i = 0; i < 10; i++)
-            {
-                if (!isConnected)
-                {
-                    // If more then 5 attempts then change connectionState
-                    SetConnectionState(i > 5 ? ConnectionState.StillConnecting : ConnectionState.Connecting);
-                
-                    try
-                    {
-                        using (var http = new HttpClient())
-                        {
-                            var response = await http.PostAsJsonAsync("https://kyh-shared.azurewebsites.net/api/devices/connect", new HttpDeviceRequest { DeviceId = deviceId });
-                            if (response.IsSuccessStatusCode)
-                            {
-                                var data = JsonConvert.DeserializeObject<HttpDeviceResponse>(await response.Content.ReadAsStringAsync());
-                                if (data != null)
-                                {
-                                    try
-                                    {
-                                        deviceClient = DeviceClient.CreateFromConnectionString(data.ConnectionString);
-
-                                        SetConnectionState(ConnectionState.Initializing);
-                                        var twinCollection = new TwinCollection();
-                                        twinCollection["owner"] = "Hans";
-                                        twinCollection["deviceType"] = "FAN";
-
-                                        await deviceClient.UpdateReportedPropertiesAsync(twinCollection);
-                                        var twin = deviceClient.GetTwinAsync();
-
-                                        var result = await http.GetAsync($"https://kyh-shared.azurewebsites.net/api/devices/connect?code=-SWiKVko3aRE3PV53iizVYEXzE84M8hpmxI39vqvXxqfAzFus0uGDA==&deviceId={deviceId}");
-                                        if (result.IsSuccessStatusCode)
-                                        {
-                                            var connectionstate = await result.Content.ReadAsStringAsync();
-                                            if (connectionstate == "Connected")
-                                            {
-                                                SetConnectionState(ConnectionState.Connected);
-                                                break;
-                                            }
-                                        }
-                                    }
-                                    catch { break; }
-                                }
-                            }
-
-                            await Task.Delay(1000);
-                        }
-
-                    }
-                    catch { }
                 }
-                else
-                    SetConnectionState(ConnectionState.Connected);
-            }
+                    
+                if (DeviceManager.ConnectionStateMessage == "Not connected")
+                {
+                    btnToggle.Visibility = Visibility.Visible;
+                    btnToggle.Content = "Connect";
+                }
 
-            if (!isConnected)
-                SetConnectionState(ConnectionState.NotConnected);
+                await Task.Delay(1000);
+            }
         }
 
 
-        private void btnToggle_Click(object sender, RoutedEventArgs e)
+        private async void btnToggle_Click(object sender, RoutedEventArgs e)
         {
-
+            if (DeviceManager.ConnectionStateMessage == "Not connected")
+                await DeviceManager.ConnectAsync();
         }
     }
 }
---
+++ Lektion-6/WpfShared/Helpers/DeviceManager.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.Azure.Devices.Client;
+using Microsoft.Azure.Devices.Shared;
+using Newtonsoft.Json;
+using Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfShared.Helpers
+{
+    public static class DeviceManager
+    {
+        public enum ConnectionState
+        {
+            NotConnected,
+            Connecting,
+            StillConnecting,
+            Initializing,
+            Connected
+        }
+
+
+        private static DeviceClient deviceClient;
+        private static bool isConnected = false;
+        private static string baseUrl = "https://kyh-shared.azurewebsites.net/api/devices/connect";
+
+        public static string ConnectionStateMessage { get; private set; }
+        public static string DeviceId { get; private set; }
+        public static string Owner { get; private set; }
+        public static string DeviceType { get; private set; }
+
+
+        public static void Initialize(string deviceId, string deviceType, string owner)
+        {
+            DeviceId = deviceId;
+            DeviceType = deviceType;
+            Owner = owner;
+        }
+
+        public static void SetConnectionState(ConnectionState state)
+        {
+            switch (state)
+            {
+                case ConnectionState.NotConnected:
+                    ConnectionStateMessage = "Not connected";
+                    break;
+
+                case ConnectionState.Connecting:
+                    ConnectionStateMessage = "Connecting. Please wait...";
+                    break;
+
+                case ConnectionState.StillConnecting:
+                    ConnectionStateMessage = "Still connecting. Please wait...";
+                    break;
+
+                case ConnectionState.Initializing:
+                    ConnectionStateMessage = "Initializing. Please wait...";
+                    break;
+
+                case ConnectionState.Connected:
+                    isConnected = true;
+                    ConnectionStateMessage = "Connected";
+                    break;
+
+            }
+
+        }
+
+        public static async Task ConnectAsync()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if (!isConnected)
+                {
+                    // If more then 5 attempts then change connectionState
+                    SetConnectionState(i > 5 ? ConnectionState.StillConnecting : ConnectionState.Connecting);
+
+                    try
+                    {
+                        using (var http = new HttpClient())
+                        {
+                            var response = await http.PostAsJsonAsync(baseUrl, new HttpDeviceRequest { DeviceId = DeviceId });
+                            if (response.IsSuccessStatusCode)
+                            {
+                                var data = JsonConvert.DeserializeObject<HttpDeviceResponse>(await response.Content.ReadAsStringAsync());
+                                if (data != null)
+                                {
+                                    try
+                                    {
+                                        deviceClient = DeviceClient.CreateFromConnectionString(data.ConnectionString);
+
+                                        SetConnectionState(ConnectionState.Initializing);
+                                        var twinCollection = new TwinCollection();
+                                        twinCollection["owner"] = Owner;
+                                        twinCollection["deviceType"] = DeviceType;
+
+                                        await deviceClient.UpdateReportedPropertiesAsync(twinCollection);
+                                        var twin = deviceClient.GetTwinAsync();
+
+                                        var result = await http.GetAsync($"{baseUrl}?code=-SWiKVko3aRE3PV53iizVYEXzE84M8hpmxI39vqvXxqfAzFus0uGDA==&deviceId={DeviceId}");
+                                        if (result.IsSuccessStatusCode)
+                                        {
+                                            var connectionstate = await result.Content.ReadAsStringAsync();
+                                            if (connectionstate == "Connected")
+                                            {
+                                                SetConnectionState(ConnectionState.Connected);
+                                                break;
+                                            }
+                                        }
+                                    }
+                                    catch { break; }
+                                }
+                            }
+
+                            await Task.Delay(1000);
+                        }
+
+                    }
+                    catch { }
+                }
+                else
+                    SetConnectionState(ConnectionState.Connected);
+            }
+
+            if (!isConnected)
+                SetConnectionState(ConnectionState.NotConnected);
+        }
+    }
+}

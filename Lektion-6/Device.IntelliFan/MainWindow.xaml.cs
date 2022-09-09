4ed8da3c97dc3fcecf00a84b6f2f8dd0710ecfa8
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2022-09-09 12:05:18 +0200

    Lektion 6 (AzureFunctions WPF Device)

--- Lektion-6/Device.IntelliFan/MainWindow.xaml.cs
+++ Lektion-6/Device.IntelliFan/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,15 +16,44 @@ using System.Windows.Shapes;
 
 namespace Device.IntelliFan
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    enum ConnectionState
+    {
+        NotConnected,
+        Connecting,
+        StillConnecting,
+        Initializing,
+        Connected
+    }
+
+
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            ConnectAsync().ConfigureAwait(false);
         }
+
+        private bool isConnected;
+
+
+        private void SetConnectionState(ConnectionState state)
+        {
+
+        }
+
+        private async Task ConnectAsync()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if (!isConnected)
+                {
+                    // If more then 3 attempts then change connectionState
+                    SetConnectionState(i > 3 ? ConnectionState.StillConnecting : ConnectionState.Connecting);
+                }
+            }
+        }
+
 
         private void btnToggle_Click(object sender, RoutedEventArgs e)
         {

1bdece584ef1d28366e7f7cd864a3df680e74b94
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2022-09-14 11:57:55 +0200

    Lektion 7 (WPF UserControl, ResourceDictionary)

---
+++ Lektion-7/SmartApp/MVVM/ViewModels/MainViewModel.cs
@@ -0,0 +1,39 @@
+﻿using SmartApp.MVVM.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartApp.MVVM.ViewModels
+{
+    internal class MainViewModel : ObservableObject
+    {
+        public MainViewModel()
+        {
+            KitchenViewModel = new KitchenViewModel();
+            CurrentView = KitchenViewModel;
+
+            KitchenViewCommand = new RelayCommand(x => { CurrentView = KitchenViewModel; });
+            
+        }
+        
+        
+        
+        private object _currentView;
+        
+        public RelayCommand KitchenViewCommand { get; set; }
+        public KitchenViewModel KitchenViewModel { get; set; }
+
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}

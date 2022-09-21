cdb5e7f89e505992cbf4ceeffb80651278a68650
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2022-09-21 12:05:25 +0200

    Lektion 9 (WPF UserControl Bindings)

--- Lektion-4/AsyncAwait.WPF/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ Lektion-4/AsyncAwait.WPF/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -77,7 +77,7 @@ namespace AsyncAwait.WPF {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/AsyncAwait.WPF;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/AsyncAwait.WPF;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
---
+++ Lektion-9/SmartApp/MVVM/ViewModels/KitchenViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartApp.MVVM.ViewModels
+{
+    internal class KitchenViewModel
+    {
+        public string Title { get; set; } = "Kitchen";
+        public string Temperature { get; set; } = "23 °C";
+        public string Humidity { get; set; } = "34 %";
+    }
+}

a6e2aca3c5b8461db407de3ec32ea0e017326ac5
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2022-10-05 14:18:16 +0200

    Lektion 13 (WCF, Final App)

--- FinalApp/Device.ConsoleApp/Program.cs
+++ FinalApp/Device.ConsoleApp/Program.cs
@@ -6,6 +6,8 @@ using Microsoft.Azure.Devices.Shared;
 
 DeviceClient deviceClient;
 var interval = 10000;
+var deviceId = "";
+var device_connectionString = "";
 var deviceName = "";
 var deviceType = "";
 var location = "";
@@ -13,24 +15,40 @@ var location = "";
 var connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\HansMattin-Lassei\\Documents\\Utbildning\\NET21\\FinalApp\\Device.ConsoleApp\\device_consoleapp_db.mdf;Integrated Security=True;Connect Timeout=30";
 using var conn = new SqlConnection(connectionString);
 
-var deviceId = await conn.QueryFirstOrDefaultAsync<string>("SELECT DeviceId FROM Settings");
+deviceId = await conn.QueryFirstOrDefaultAsync<string>("SELECT DeviceId FROM Settings");
 if (string.IsNullOrEmpty(deviceId))
 {
     deviceId = Guid.NewGuid().ToString();
- 
-    using var client = new HttpClient();
-    var result = await client.PostAsJsonAsync("http://localhost:7118/api/devices/connect", new { deviceId });
-    var device_connectionString = await result.Content.ReadAsStringAsync();
+    Console.Write("Enter Device Name: ");
+    deviceName = Console.ReadLine();
+    Console.Write("Enter Device Type: ");
+    deviceType = Console.ReadLine();
+    Console.Write("Enter Location: ");
+    location = Console.ReadLine();
 
-    deviceClient = DeviceClient.CreateFromConnectionString(device_connectionString, TransportType.Mqtt);
-    var twin = await deviceClient.GetTwinAsync();
-    try { interval = (int)twin.Properties.Desired["interval"]; }
-    catch { }
-
-    var twinCollection = new TwinCollection();
-    twinCollection["deviceName"] = deviceName;
-    twinCollection["deviceType"] = deviceType;
-    twinCollection["location"] = location;
-
-    await deviceClient.UpdateReportedPropertiesAsync(twinCollection);
+    await conn.ExecuteAsync("INSERT INTO Settings VALUES (@DeviceId, @ConnectionString, @DeviceName, @DeviceType, @Location)", new
+    {
+        DeviceId = deviceId,
+        ConnectionString = device_connectionString,
+        DeviceName = deviceName,
+        DeviceType = deviceType,
+        Location = location
+    });
 }
+
+using var client = new HttpClient();
+var result = await client.PostAsJsonAsync("http://localhost:7118/api/devices/connect", new { deviceId });
+device_connectionString = await result.Content.ReadAsStringAsync();
+
+deviceClient = DeviceClient.CreateFromConnectionString(device_connectionString, TransportType.Mqtt);
+var twin = await deviceClient.GetTwinAsync();
+try { interval = (int)twin.Properties.Desired["interval"]; }
+catch { }
+
+var twinCollection = new TwinCollection();
+twinCollection["deviceName"] = deviceName;
+twinCollection["deviceType"] = deviceType;
+twinCollection["location"] = location;
+
+await deviceClient.UpdateReportedPropertiesAsync(twinCollection);
+
--- FinalApp/SmartApp/App.xaml.cs
+++ FinalApp/SmartApp/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -8,10 +10,37 @@ using System.Windows;
 
 namespace SmartApp
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
+    /* 
+         Singleton = en endaste instans av någonting i hela programmet. Tills vi startar om Servern/Webservern
+         Scoped = en ny instans för varje gång vi går in på en kontroller men våra partial views får samma information
+         Transient = en ny instans för varje sak vi gör, avsett vad
+    */
     public partial class App : Application
     {
+        public static IHost? app { get; private set; }
+
+        public App()
+        {
+            app = Host.CreateDefaultBuilder().ConfigureServices((context, services) =>
+            {
+                services.AddSingleton<MainWindow>();
+                
+            }).Build();
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            await app!.StartAsync();
+            var MainWindow = app.Services.GetRequiredService<MainWindow>();
+            MainWindow.Show();
+
+            base.OnStartup(e);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            await app!.StopAsync();
+            base.OnExit(e);
+        }
     }
 }
